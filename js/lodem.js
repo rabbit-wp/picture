0f730f0951c77be68ebb32057fd5521050077bdf
李明 <q1345609248@gmail.com>
2020-08-19 05:04:52 +0800

    1

---
+++ js/lodem.js
@@ -0,0 +1,12 @@
+    function copyText() {
+    var d=document.getElementById("myDivId_new");
+    if(d==null){
+      document.getElementById('mao1').innerHTML = '改写成功';
+        var text = document.getElementById("mao1").innerText;
+          inputed.value = text;
+          inputed.select();
+          document.execCommand("copy");
+          document.getElementById("myDivId").id = 'myDivId_new';
+    }else{
+}
+    }
