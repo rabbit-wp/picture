3ba61f0e276a8fc4da095bd1c1bc1b9efcd83280
李明 <q1345609248@gmail.com>
2020-09-11 19:46:32 +0800

    Update zzzces.js

--- js/dow/zzzces.js
+++ js/dow/zzzces.js
@@ -1,13 +1,5 @@
-document.write("<div>");
-document.write("<a href=\"mttbrowser:\/\/url=https:\/\/forstar.gitee.io\/page\/\">qqbrowser<\/a><\/br>");
-document.write("<a href=\"baiduboxapp:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">baidu<\/a><\/br>");
-document.write("<a href=\"googlechrome:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">googlechrome<\/a><\/br>");
-document.write("<a href=\"ucweb:\/\/forstar.gitee.io\/page\/\">uc<\/a><\/br>");
-document.write("<a href=\"mibrowser:\/\/forstar.gitee.io\/page\/\">mibrowser<\/a><\/br>");
-document.write("<a href=\"taobao:\/\/forstar.gitee.io\/page\/\">taobao<\/a><\/br>");
-document.write("<a href=\"qihoobrowser:\/\/forstar.gitee.io\/page\/\">qihoobrowser<\/a><\/br>");
-document.write("<a href=\"alipays:\/\/platformapi\/startapp?appId=20000067&url=http%3A%2F%2Fforstar.gitee.io\/page\/\">alipay<\/a>");
-document.write("<\/div>");
+document.write("<div><a href=\"mttbrowser:\/\/url=https:\/\/forstar.gitee.io\/page\/\">qqbrowser<\/a><br><a href=\"baiduboxapp:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">baidu<\/a><br><a href=\"googlechrome:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">googlechrome<\/a><br><a href=\"ucweb:\/\/forstar.gitee.io\/page\/\">uc<\/a><br><a href=\"mibrowser:\/\/forstar.gitee.io\/page\/\">mibrowser<\/a><br><a href=\"taobao:\/\/forstar.gitee.io\/page\/\">taobao<\/a><br><a href=\"qihoobrowser:\/\/forstar.gitee.io\/page\/\">qihoobrowser<\/a><br><a href=\"alipays:\/\/platformapi\/startapp?appId=20000067&url=http%3A%2F%2Fforstar.gitee.io\/page\/\">alipay<\/a><\/div>");
+
 
 /* <div>
 <a href="mttbrowser://url=https://forstar.gitee.io/page/">qqbrowser</a></br>
