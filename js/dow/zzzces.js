3eb3c6464a05d7031fd0044582a1687758cd1ea5
李明 <q1345609248@gmail.com>
2020-09-11 19:29:24 +0800

    Update zzzces.js

--- js/dow/zzzces.js
+++ js/dow/zzzces.js
@@ -1,8 +1,10 @@
-document.write("<a href=\"mttbrowser:\/\/url=https:\/\/forstar.gitee.io\/page\/\">qqbrowser<\/a>");
-document.write("<a href=\"baiduboxapp:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">baidu<\/a>");
-document.write("<a href=\"googlechrome:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">googlechrome<\/a>");
-document.write("<a href=\"ucweb:\/\/forstar.gitee.io\/page\/\">uc<\/a>");
-document.write("<a href=\"mibrowser:\/\/forstar.gitee.io\/page\/\">mibrowser<\/a>");
-document.write("<a href=\"taobao:\/\/forstar.gitee.io\/page\/\">taobao<\/a>");
-document.write("<a href=\"qihoobrowser:\/\/forstar.gitee.io\/page\/\">qihoobrowser<\/a>");
-document.write("<a href=\"alipays:\/\/platformapi\/startapp?appId=20000067&url=http%3A%2F%2Fforstar.gitee.io\/page\/\">alipay<\/a>");
+document.write("<a href=\"mttbrowser:\/\/url=https:\/\/forstar.gitee.io\/page\/\">qqbrowser<\/a><a href=\"baiduboxapp:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">baidu<\/a><a href=\"googlechrome:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">googlechrome<\/a><a href=\"ucweb:\/\/forstar.gitee.io\/page\/\">uc<\/a><a href=\"mibrowser:\/\/forstar.gitee.io\/page\/\">mibrowser<\/a><a href=\"taobao:\/\/forstar.gitee.io\/page\/\">taobao<\/a><a href=\"qihoobrowser:\/\/forstar.gitee.io\/page\/\">qihoobrowser<\/a><a href=\"alipays:\/\/platformapi\/startapp?appId=20000067&url=http%3A%2F%2Fforstar.gitee.io\/page\/\">alipay<\/a>");
+
+{/* <a href="mttbrowser://url=https://forstar.gitee.io/page/">qqbrowser</a>
+<a href="baiduboxapp://browse?url=https://forstar.gitee.io/page/">baidu</a>
+<a href="googlechrome://browse?url=https://forstar.gitee.io/page/">googlechrome</a>
+<a href="ucweb://forstar.gitee.io/page/">uc</a>
+<a href="mibrowser://forstar.gitee.io/page/">mibrowser</a>
+<a href="taobao://forstar.gitee.io/page/">taobao</a>
+<a href="qihoobrowser://forstar.gitee.io/page/">qihoobrowser</a>
+<a href="alipays://platformapi/startapp?appId=20000067&url=http%3A%2F%2Fforstar.gitee.io/page/">alipay</a> */}
