24b595639588b661d30cc926890a2752fe8a8c92
李明 <q1345609248@gmail.com>
2020-09-11 19:50:19 +0800

    Update zzzces.js

--- js/dow/zzzces.js
+++ js/dow/zzzces.js
@@ -1,13 +1,13 @@
 document.write("<div><a href=\"mttbrowser:\/\/url=https:\/\/forstar.gitee.io\/page\/\">qqbrowser<\/a><br><a href=\"baiduboxapp:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">baidu<\/a><br><a href=\"googlechrome:\/\/browse?url=https:\/\/forstar.gitee.io\/page\/\">googlechrome<\/a><br><a href=\"ucweb:\/\/forstar.gitee.io\/page\/\">uc<\/a><br><a href=\"mibrowser:\/\/forstar.gitee.io\/page\/\">mibrowser<\/a><br><a href=\"taobao:\/\/forstar.gitee.io\/page\/\">taobao<\/a><br><a href=\"qihoobrowser:\/\/forstar.gitee.io\/page\/\">qihoobrowser<\/a><br><a href=\"alipays:\/\/platformapi\/startapp?appId=20000067&url=http%3A%2F%2Fforstar.gitee.io\/page\/\">alipay<\/a><\/div>");
 
 
-/* <div>
-<a href="mttbrowser://url=https://forstar.gitee.io/page/">qqbrowser</a></br>
-<a href="baiduboxapp://browse?url=https://forstar.gitee.io/page/">baidu</a></br>
-<a href="googlechrome://browse?url=https://forstar.gitee.io/page/">googlechrome</a></br>
-<a href="ucweb://forstar.gitee.io/page/">uc</a></br>
-<a href="mibrowser://forstar.gitee.io/page/">mibrowser</a></br>
-<a href="taobao://forstar.gitee.io/page/">taobao</a></br>
-<a href="qihoobrowser://forstar.gitee.io/page/">qihoobrowser</a></br>
+<div>
+<a href="mttbrowser://url=https://forstar.gitee.io/page/">qqbrowser&ensp;</a>
+<a href="baiduboxapp://browse?url=https://forstar.gitee.io/page/">baidu&ensp;</a>
+<a href="googlechrome://browse?url=https://forstar.gitee.io/page/">googlechrome&ensp;</a>
+<a href="ucweb://forstar.gitee.io/page/">uc&ensp;</a>
+<a href="mibrowser://forstar.gitee.io/page/">mibrowser&ensp;</a>
+<a href="taobao://forstar.gitee.io/page/">taobao&ensp;</a>
+<a href="qihoobrowser://forstar.gitee.io/page/">qihoobrowser&ensp;</a>
 <a href="alipays://platformapi/startapp?appId=20000067&url=http%3A%2F%2Fforstar.gitee.io/page/">alipay</a>
-</div> */
+</div>
