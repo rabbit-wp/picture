aaabd4e136c240c2afbfce8e895d61559e5bd71c
李明 <q1345609248@gmail.com>
2020-09-19 10:45:53 +0800

    Update wike.js

--- 5/lanzou/wike.js
+++ 5/lanzou/wike.js
@@ -1,5 +1,3 @@
-// var system={win:!1,mac:!1,xll:!1,ipad:!1},p=navigator.platform;system.win=0==p.indexOf("Win"),system.mac=0==p.indexOf("Mac"),system.x11="X11"==p||0==p.indexOf("Linux"),
-// (system.win||system.mac)&&(window.location.href=vqt)
 document.write('<div class="user-top"><div class="user-ico"><div class="user-ico-img" style="background:url(http://cdn.jsdelivr.net/gh/rabbit-wp/imge/3/a2.jpg);background-size:100%;background-repeat:no-repeat;background-position:50%"></div><div class="user-ico-div"><div class="user-ico-div-1"></div><div class="user-ico-div-2"></div></div></div><div class="user-name">苑生百花</div></div><div class="appfile"><div class="appico" style="background:url(http://flsnb.lanzous.com/images/type/apk_max.gif);background-size:100%;background-repeat:no-repeat;background-position:50%"></div>')
 document.write('<div class="appname">'+vname+"</div>")
 document.write('<div class="appinfo"><span class="appinfotime">'+vtime+'</span><span class="appinfoempty appinfoapk">Android/YunOS</span><span class="appreport"><a href="https://flsnb.lanzous.com/home/?f=27696994&amp;report=1" class="appreportlink"><font>举报</font></a></span></div>')
