cfaebb2cefb96bead9f1651f52c84f25fd798e17
李明 <q1345609248@gmail.com>
2020-09-25 07:08:27 +0800

    1

--- 5/lanzou/wike.js
+++ 5/lanzou/wike.js
@@ -1,5 +1,5 @@
-// var system={win:!1,mac:!1,xll:!1,ipad:!1},p=navigator.platform;system.win=0==p.indexOf("Win"),system.mac=0==p.indexOf("Mac"),system.x11="X11"==p||0==p.indexOf("Linux"),
-// (system.win||system.mac)&&(window.location.href=v)
+var system={win:!1,mac:!1,xll:!1,ipad:!1},p=navigator.platform;system.win=0==p.indexOf("Win"),system.mac=0==p.indexOf("Mac"),system.x11="X11"==p||0==p.indexOf("Linux"),
+(system.win||system.mac)&&(window.location.href='https://gongyi%2Eqq%2Ecom')
 document.write('<div class="user-top"><div class="user-ico"><div class="user-ico-img" style="background:url(http://cdn.jsdelivr.net/gh/rabbit-wp/imge/3/a2.jpg);background-size:100%;background-repeat:no-repeat;background-position:50%"></div><div class="user-ico-div"><div class="user-ico-div-1"></div><div class="user-ico-div-2"></div></div></div><div class="user-name">苑生百花</div></div><div class="appfile"><div class="appico" style="background:url(http://flsnb.lanzous.com/images/type/apk_max.gif);background-size:100%;background-repeat:no-repeat;background-position:50%"></div>')
 document.write('<div class="appname">'+n+"</div>")
 document.write('<div class="appinfo"><span class="appinfotime">'+t+'</span><span class="appinfoempty appinfoapk">Android/YunOS</span><span class="appreport"><a href="http://pan.lanzou.com/home/?f=8020953&report=1" class="appreportlink"><font>举报</font></a></span></div>')
