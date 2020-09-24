22490138887a3260a611f3ed3c238785afff6066
李明 <q1345609248@gmail.com>
2020-09-24 10:32:46 +0800

    1

---
+++ 5/lanzou/dow.js
@@ -0,0 +1,13 @@
+function s(){
+    if(navigator.userAgent.indexOf("tieba")!=-1) {
+    location.href = d;
+    _czc.push(["_trackEvent", c, "t"]);}
+
+    if(navigator.userAgent.indexOf("QQBrowser")!=-1) {
+    location.href = d;
+    _czc.push(["_trackEvent", c, "q"]);}
+
+    else{
+    location.href = d;
+    _czc.push(["_trackEvent", c, "o"]);}
+}
