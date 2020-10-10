4314703a7e4f2b0e3c4264996fe93eb33181fb58
李明 <q1345609248@gmail.com>
2020-10-10 09:16:34 +0800

    34534

--- 5/lanzou/mains.js
+++
@@ -1,5 +0,0 @@
-var ua = navigator.userAgent.toLowerCase();
-if(ua.indexOf('pa qq') > -1){var b="mttbrowser://url=";}
-else{var b ="taobao://";}
-
-ready1.onclick = function() {top.location.href= b+"bilibili.com";}
