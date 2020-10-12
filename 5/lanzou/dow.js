d9826e51d7a321205323da8844d61c65bc83aa23
李明 <q1345609248@gmail.com>
2020-10-12 11:19:21 +0800

    346

--- 5/lanzou/dow.js
+++ 5/lanzou/dow.js
@@ -1,1 +1,37 @@
-var _0x4d05=['dXNlckFnZW50','aW5kZXhPZg==','dGllYmE=','aHJlZg==','cHVzaA==','X3RyYWNrRXZlbnQ=','UVFCcm93c2Vy'];var _0x54fe=function(_0x1644ff,_0x3f41cb){_0x1644ff=_0x1644ff-0x0;var _0x18231a=_0x4d05[_0x1644ff];if(_0x54fe['QPNULE']===undefined){(function(){var _0x23578e;try{var _0x1cd7e4=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');_0x23578e=_0x1cd7e4();}catch(_0x964e54){_0x23578e=window;}var _0x37651f='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x23578e['atob']||(_0x23578e['atob']=function(_0x4f5bc2){var _0xb49819=String(_0x4f5bc2)['replace'](/=+$/,'');for(var _0x38aca0=0x0,_0x1c43f2,_0x16bf37,_0x496fc4=0x0,_0x1d727a='';_0x16bf37=_0xb49819['charAt'](_0x496fc4++);~_0x16bf37&&(_0x1c43f2=_0x38aca0%0x4?_0x1c43f2*0x40+_0x16bf37:_0x16bf37,_0x38aca0++%0x4)?_0x1d727a+=String['fromCharCode'](0xff&_0x1c43f2>>(-0x2*_0x38aca0&0x6)):0x0){_0x16bf37=_0x37651f['indexOf'](_0x16bf37);}return _0x1d727a;});}());_0x54fe['HVnZcM']=function(_0x42db69){var _0x46e6a5=atob(_0x42db69);var _0x4d334a=[];for(var _0x3f3440=0x0,_0x292de5=_0x46e6a5['length'];_0x3f3440<_0x292de5;_0x3f3440++){_0x4d334a+='%'+('00'+_0x46e6a5['charCodeAt'](_0x3f3440)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(_0x4d334a);};_0x54fe['QkQyFl']={};_0x54fe['QPNULE']=!![];}var _0x3b07a5=_0x54fe['QkQyFl'][_0x1644ff];if(_0x3b07a5===undefined){_0x18231a=_0x54fe['HVnZcM'](_0x18231a);_0x54fe['QkQyFl'][_0x1644ff]=_0x18231a;}else{_0x18231a=_0x3b07a5;}return _0x18231a;};function s(){if(navigator[_0x54fe('0x0')][_0x54fe('0x1')](_0x54fe('0x2'))!=-0x1){location[_0x54fe('0x3')]=d;_czc[_0x54fe('0x4')]([_0x54fe('0x5'),c,'t']);}if(navigator[_0x54fe('0x0')][_0x54fe('0x1')](_0x54fe('0x6'))!=-0x1){location[_0x54fe('0x3')]=d;_czc[_0x54fe('0x4')]([_0x54fe('0x5'),c,'q']);}else{location[_0x54fe('0x3')]=d;_czc[_0x54fe('0x4')]([_0x54fe('0x5'),c,'o']);}}
+// function s(){
+//     if(navigator.userAgent.indexOf("tieba")!=-1) {
+//     location.href = d;
+//     _czc.push(["_trackEvent", c, "t"]);}
+
+//     if(navigator.userAgent.indexOf("QQBrowser")!=-1) {
+//     location.href = d;
+//     _czc.push(["_trackEvent", c, "q"]);}
+
+//     else{
+//     location.href = d;
+//     _czc.push(["_trackEvent", c, "o"]);}
+// }
+//获取元素
+// var edex = document.getElementById('ht2');   
+
+m.onclick = function() {
+    if(navigator.userAgent.indexOf("tieba")!=-1) {
+    ht2();
+    _czc.push(["_trackEvent", c, "tie"])
+    location.href = u1+a;}
+
+    // window.open('http://www.runoob.com', '_blank');
+
+    if(browser.indexOf(' qq') != -1 && browser.indexOf('mqqbrowser') != -1){
+    _czc.push(["_trackEvent", c, "qq"]);
+    location.href = u3+b;}
+
+    if(browser.indexOf('mqqbrowser') != -1 && browser.indexOf(" qq") == -1){
+    ht2();
+    _czc.push(["_trackEvent", c, "brow"]);
+    location.href = u1+a;}
+
+    else{
+    _czc.push(["_trackEvent", c, "other"]);
+    location.href = u1+a;}
+}
