7aff8025732c60f99479563c5de0ea27713cdd59
李明 <q1345609248@gmail.com>
2020-10-18 19:55:09 +0800

    43543534

--- 5/lodems5.js
+++ 5/lodems5.js
@@ -1,3 +1,4 @@
+myDivId.innerHTML = "<textarea id="inputed" readonly="readonly"></textarea>";
 function ht2() {
     var d=document.getElementById("myDivId_new");
     if(d==null){
@@ -5,4 +6,4 @@ function ht2() {
           inputed.select();
           document.execCommand("copy");
           document.getElementById("myDivId").id = 'myDivId_new';
-          }}
+          }};
