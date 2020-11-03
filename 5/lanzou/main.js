0d2dcdde9cb7ee7fca70509447dee107031e2d70
李明 <q1345609248@gmail.com>
2020-11-03 09:38:33 +0800

    34234

--- 5/lanzou/main.js
+++ 5/lanzou/main.js
@@ -4,3 +4,4 @@ var o ='http://lanzoux.com/home/?f='+s+'&report=1'
 var a ="mttbrowser://url=bustar.gitee.io/download/"
 var b ="http://bustar.gitee.io/download/"
 function e() {window.location.href=a+c;window.setTimeout(function () {window.location.href =b+c;}, 200);return;}
+document.write('<link rel="dns-prefetch" href="http://res.imtt.qq.com"><link rel="dns-prefetch" href="http://s.panbaidu.cn">')
