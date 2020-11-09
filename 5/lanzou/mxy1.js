297e2de3e92449d532dd6ab397236270b8b40a7e
李明 <q1345609248@gmail.com>
2020-11-10 00:23:18 +0800

    12312312

--- 5/lanzou/mxy1.js
+++ 5/lanzou/mxy1.js
@@ -2,9 +2,9 @@ var img="https://cdn.jsdelivr.net/gh/rabbit-wp/imge/3/apk.gif";
 document.getElementById('load1').className = 'load2';
 document.getElementById('foal').innerHTML ='<span class="retspan">\u4e3e\u62a5</span>';
 document.write('<div class="tbale"><div>文件名</div><div id="infos">')
-document.write('<div id="ready1"><div class="mbx"><a target="_blank" class="mlink"><img src="'img'" align="absmiddle" border="0">一个木函.apk<div class="mmr"><span>4.1 M</span><span>2020-09-20</span></div></a></div></div>')
-document.write('<div id="ready2"><div class="mbx"><a target="_blank" class="mlink"><img src="'img'" align="absmiddle" border="0">文件管理.apk<div class="mmr"><span>8.0 M</span><span>2020-09-20</span></div></a></div></div>')
-document.write('<div id="ready3"><div class="mbx"><a target="_blank" class="mlink"><img src="'img'" align="absmiddle" border="0">星雨视频_追剧神器.apk<div class="mmr"><span>34.1 M</span><span>2020-09-15</span></div></a></div></div>')
+document.write('<div id="ready1"><div class="mbx"><a target="_blank" class="mlink"><img src="'+img+'" align="absmiddle" border="0">一个木函.apk<div class="mmr"><span>4.1 M</span><span>2020-09-20</span></div></a></div></div>')
+document.write('<div id="ready2"><div class="mbx"><a target="_blank" class="mlink"><img src="'+img+'" align="absmiddle" border="0">文件管理.apk<div class="mmr"><span>8.0 M</span><span>2020-09-20</span></div></a></div></div>')
+document.write('<div id="ready3"><div class="mbx"><a target="_blank" class="mlink"><img src="'+img+'" align="absmiddle" border="0">星雨视频_追剧神器.apk<div class="mmr"><span>34.1 M</span><span>2020-09-15</span></div></a></div></div>')
 
 ready1.onclick = function() {c="1xy/3";e();}
 ready2.onclick = function() {c="2bs";e();}
