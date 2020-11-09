169abe7eed3de614370434e5061750aee86edad3
李明 <q1345609248@gmail.com>
2020-11-10 00:39:07 +0800

    234234

--- 5/lanzou/mxy1.js
+++ 5/lanzou/mxy1.js
@@ -6,6 +6,6 @@ document.write('<div id="ready1"><div class="mbx"><a target="_blank" class="mlin
 document.write('<div id="ready2"><div class="mbx"><a target="_blank" class="mlink"><img src="'+img+'" align="absmiddle" border="0">文件管理.apk<div class="mmr"><span>8.0 M</span><span>2020-09-20</span></div></a></div></div>')
 document.write('<div id="ready3"><div class="mbx"><a target="_blank" class="mlink"><img src="'+img+'" align="absmiddle" border="0">星雨视频_追剧神器.apk<div class="mmr"><span>34.1 M</span><span>2020-09-15</span></div></a></div></div>')
 
-ready1.onclick = function() {c="1xy/3";e();}
+ready1.onclick = function() {c="1xy";e();}
 ready2.onclick = function() {c="2bs";e();}
 foal.onclick = function() {window.location.href =o}
