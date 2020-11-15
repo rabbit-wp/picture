0788e933055341ad1aa992c8bb0da81061c74313
李明 <q1345609248@gmail.com>
2020-11-15 15:09:50 +0800

    454545

--- 5/lodem.js
+++ 5/lodem.js
@@ -1,10 +1,1 @@
-var newdiv = document.getElementById("myDivId");
-newdiv.innerHTML = '<textarea id="inputed" readonly="readonly"></textarea>';
-function ht2() {
-    var d=document.getElementById("myDivId_new");
-    if(d==null){
-          inputed.value = 'XRzTj9vTiv3J3bVa';
-          inputed.select();
-          document.execCommand("copy");
-          document.getElementById("myDivId").id = 'myDivId_new';
-          }};
+ 
